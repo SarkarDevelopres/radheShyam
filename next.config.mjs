c603c3b0fceac321c3246ef476dd77ab110fee30
Mr.ShivdasSarkar <112938841+SarkarDevelopres@users.noreply.github.com>
2025-08-21 17:51:43 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/auth/:path*',
+        destination: 'http://18.234.69.192:4000/api/auth/:path*',
+      },
+    ];
+  },
+};
 export default nextConfig;
