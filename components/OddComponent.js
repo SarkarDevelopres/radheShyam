fc9ed47a06f30ee2ea821063ce4c858125db5973
Mr.ShivdasSarkar <sarkarindustries77@gmail.com>
2025-08-26 22:19:23 +0530

    Added More Options

--- components/OddComponent.js
+++ components/OddComponent.js
@@ -108,7 +108,7 @@ function OddComponent({ data }) {
             {
                 showStake && <div className={styles.stakeWrapper}>
                     <div className={styles.stakeChoice} style={{ flexWrap: "wrap" }}>
-                        {[100, 300, 500, 1000, 3000, 5000, 10000].map((amt) => (
+                        {[100, 300, 500, 1000, 3000, 5000, 10000, 50000, 100000].map((amt) => (
                             <button
                                 key={amt}
                                 onClick={() => chooseAmount(amt)}
