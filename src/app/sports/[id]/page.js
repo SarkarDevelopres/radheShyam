15d796c9e96f2ff282566e3e51666f10029839df
Mr.ShivdasSarkar <sarkarindustries77@gmail.com>
2025-09-09 20:49:31 +0530

    added live Score & Odds

--- src/app/sports/[id]/page.js
+++ src/app/sports/[id]/page.js
@@ -131,7 +131,12 @@ function GameComp() {
   const [isLoading, setIsLoading] = useState(true);
   const [oddsData, setOddsData] = useState([]);
   const [metaData, setMetaData] = useState([]);
-  const [liveData, setLiveData] = useState({})
+  const [liveData, setLiveData] = useState({
+    runs:0,
+    wickets:0,
+    overs:0.0
+  })
+  const [isLive, setIsLive] = useState(false)
   const openTV = () => {
     if (!tvOn) {
       tvRef.current.style.height = "200px";
@@ -205,42 +210,67 @@ function GameComp() {
     let id = localStorage.getItem("matchId");
     let status = localStorage.getItem("status");
 
-    // if (status == "live") {
-    //   if (!id) return;
+    setIsLive(true)
 
-    //   const base = process.env.NEXT_PUBLIC_SERVER_PORT;
-    //   const socket = io(base, { transports: ["websocket"] });
+    if (status == "live") {
+      if (!id) return;
 
-    //   socket.on("connect", () => console.log("[socket] connected", socket.id));
-    //   socket.onAny((ev, ...args) => {
-    //     const payload = args[0]; // usually the data object
-    //     console.log("[socket:any]", ev, payload);
+      const base = process.env.NEXT_PUBLIC_SERVER_PORT;
+      const socket = io(base, { transports: ["websocket"] });
 
-    //     if (payload?.response?.live) {
-    //       setLiveData(payload.response.live);
-    //     }
+      socket.on("connect", () => console.log("[socket] connected", socket.id));
+      socket.onAny((...args) => {
+        const payload = [...args];
+        // usually the data object
+        console.log("[socket:any]", payload);
+        // console.log(Array.isArray(payload));
+        // let teamAOdds = payload.data?.liveodds
+        // if (payload?.response?.live) {
+        //   setLiveData(payload.response.live);
+        // }
 
-    //     if (payload?.response?.live_odds?.matchodds) {
-    //       setOddsData((prev) => {
-    //         const next = { ...prev };
-    //         if (next.outcomes) {
-    //           next.outcomes = [...next.outcomes];
-    //           next.outcomes[0] = { ...next.outcomes[0], price: payload.response.live_odds.matchodds.teama.back };
-    //           next.outcomes[1] = { ...next.outcomes[1], price: payload.response.live_odds.matchodds.teamb.back };
-    //         }
-    //         return next;
-    //       });
-    //     }
-    //   });
-    //   socket.emit("watch:join", id);
-    //   socket.on("score:update", (d) => console.log("Live score update:", d));
-    //   setIsLoading(false)
-    //   return () => {
-    //     socket.emit("watch:leave", id);
-    //     socket.disconnect();
-    //   };
-    // }
-    // else {
+        // if (payload[1].kind="snapshot") {
+        //   setOddsData((prev) => {
+        //     const next = { ...prev };
+        //     if (next.outcomes) {
+        //       next.outcomes = [...next.outcomes];
+        //       next.outcomes[0] = { ...next.outcomes[0], price: payload.data.liveOdds.matchodds.teama.back };
+        //       next.outcomes[1] = { ...next.outcomes[1], price:payload.data.liveOdds.matchodds.teamb.back };
+        //     }
+        //     return next;
+        //   });
+        // }
+      });
+      socket.emit("watch:join", id);
+      socket.on("score:update", (d) => {
+        let teamAOdds = d.data?.liveOdds.matchodds.teama.back
+        console.log(teamAOdds);
+
+        if (d?.data?.liveScore) {
+          setLiveData(d.data.liveScore);
+        }
+
+        if (d.kind == "snapshot") {
+          setIsLoading(false)
+          setOddsData([{
+            outcomes: [{
+              name: d.data.teamData.teama,
+              price: d.data.liveOdds.matchodds.teama.back
+            },
+            {
+              name: d.data.teamData.teamb,
+              price: d.data.liveOdds.matchodds.teamb.back
+            },]
+          }]);
+        }
+        console.log("Live score update:", d)
+      });
+      return () => {
+        socket.emit("watch:leave", id);
+        socket.disconnect();
+      };
+    }
+    else {
 
       // Run immediately on mount
       fetchData(id);
@@ -252,7 +282,7 @@ function GameComp() {
       setIsLoading(false)
       // Clear interval on unmount
       return () => clearInterval(interval);
-    // }
+    }
   }, []);
 
   return (
@@ -278,17 +308,17 @@ function GameComp() {
         </div>
       </div>
       <div className={styles.topScoreDiv}>
-        <div className={styles.maskDiv}>
-          <span style={{position:"absolute",fontSize:"1.3rem"}}>Match not started yet</span>
+        {!isLive&&<div className={styles.maskDiv}>
+          <span style={{ position: "absolute", fontSize: "1.3rem" }}>Match not started yet</span>
           <Spinner style={{ display: "block" }} className={styles.spinnerScore} />
-        </div>
+        </div>}
         <div className={styles.teamnameDiv}>
           <h2>{oddsData[0]?.outcomes[0]?.name}</h2>
           {/* <span>Batting</span> */}
         </div>
         <div className={styles.ballsOverDiv}>
           <div className={styles.runs}>
-            0/0
+            {`${liveData.runs}/${liveData.wickets}`}
           </div>
           <div className={styles.balls}>
             <span>0</span>
@@ -299,7 +329,7 @@ function GameComp() {
             <span>0</span>
           </div>
           <div className={styles.over}>
-            0.0
+            {liveData.overs}
           </div>
         </div>
         <div className={styles.teamnameDiv}>
