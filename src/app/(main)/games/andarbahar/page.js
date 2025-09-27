a5e9751337e0ae716dc0b608e64be5624b07fcf7
Mr.ShivdasSarkar <sarkarindustries77@gmail.com>
2025-09-27 13:14:49 +0530

    text-align center for  dummy game pages

--- src/app/(main)/games/andarbahar/page.js
+++ src/app/(main)/games/andarbahar/page.js
@@ -5,7 +5,7 @@ function page() {
     <div style={{width:"100%",height:"90vh", display:"flex", justifyContent:"center",flexDirection:"column",alignItems:"center", padding:"200px 50px"}}>
         {/* <Spinner style={{fontSize:"30px", height:"100px", width:"100px"}}></Spinner> */}
         <img width={100} height={100} src={'../loading_lab.gif'}/>
-        <h2 style={{color:"#02D4F4"}}>Game is under construction...</h2>
+        <h2 style={{color:"#02D4F4", textAlign:"center"}}>Game is under construction...</h2>
     </div>
   )
 }
